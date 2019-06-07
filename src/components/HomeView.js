c8b2b14819a848bdcc7771b9ffe832908ddc44ce
Mynameisbt <mynameisbt@hotmail.com>
2019-06-07 10:49:23 -0400

    Basic header

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -4,16 +4,48 @@ import { logoutUser } from '../actions/authenticate'
 
 
 class HomeView extends Component {
+    state = {
+        selectedTab: 'Home'
+    }
     logout = (e) => {
         this.props.dispatch(logoutUser())
     }
+    
+    goTab = (tab) => {
+        this.setState(() => ({
+            selectedTab: tab
+        }));
+    }
+
+
     render() {
         console.log(this.props)
         console.log(this.state)
         return (
             <div>
                 <h3>Home View</h3>
-                <button onClick={this.logout}>Logout</button>
+                <div>
+                    <span>
+                        <button onClick={() => this.goTab('Home')}>Home</button>
+                        <button onClick={() => this.goTab('New Question')}>New Question</button>
+                        <button onClick={() => this.goTab('Leader Board')}>Leader Board</button>
+                        <span>Welcome {this.props.currentUser}</span>
+                        <button onClick={this.logout}>Logout</button>
+                    </span>
+                </div>
+                <div>
+                    {
+                        (this.state.selectedTab === 'Home') ? 
+                            <div>Home</div>
+                        :
+                        (this.state.selectedTab === 'New Question') ? 
+                            <div>New Question</div>
+                        :
+                        (this.state.selectedTab === 'Leader Board') ? 
+                            <div>Leader Board</div>
+                        : null
+                    }   
+                </div>
             </div>
         )
     }
@@ -21,7 +53,8 @@ class HomeView extends Component {
 
 function mapStateToProps ( {authenticate}) {
     return {
-        users: authenticate.users ? authenticate.users : {}
+        users: authenticate.users ? authenticate.users : {},
+        currentUser: authenticate.currentUser
     }
 }
 
