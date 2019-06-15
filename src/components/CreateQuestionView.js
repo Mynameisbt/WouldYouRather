bda4caa5918591b32a58e86667e856bb08693ac6
Mynameisbt <mynameisbt@hotmail.com>
2019-06-14 23:03:56 -0400

    Redirect after question is created

--- src/components/CreateQuestionView.js
+++ src/components/CreateQuestionView.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleCreateQuestion } from '../actions/shared'
+import { Redirect } from 'react-router-dom'
 
 class CreateQuestionsView extends Component {
+    state = {
+        toHome: false,
+      }
     create = (e) => {
         let optionOneText = document.getElementsByName("optionOne")[0].value.trim();
         let optionTwoText = document.getElementsByName("optionTwo")[0].value.trim();
         if (optionOneText.length > 0 && optionTwoText.length > 0) {
             let question = { optionOneText, optionTwoText, author: this.props.currentUser.id }
             this.props.dispatch(handleCreateQuestion(question))
+            this.setState( () => (
+                {
+                    toHome:true
+                }
+            ))
         } else {
             alert("Enter text for both")
         }
@@ -17,6 +26,9 @@ class CreateQuestionsView extends Component {
     render() {
         console.log(this.props)
         console.log(this.state)
+        if (this.state.toHome) {
+            return <Redirect to='/' />
+        }
         return (
             <div className='question-panel'>
                 <h3>Create New Question</h3>
@@ -25,6 +37,7 @@ class CreateQuestionsView extends Component {
                 <h4>OR</h4>
                 <div><input type="text" name="optionTwo"></input></div>
                 <button onClick={this.create}>Create</button>
+                
             </div>
         )
     }
